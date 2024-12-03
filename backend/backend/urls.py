9f7d75c0d1d05b59aa4525550536f261fdf424cb
YChaoCode <yichaowang639@gmail.com>
2024-12-03 13:14:04 +0800

    feature: create filter_cafes_by_labels functions

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from cafeInfo import views as cafe_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("cafes/", cafe_views.get_all_cafes, name="get_all_cafes"),
+    path("cafe/", cafe_views.get_cafe, name="get_cafe"),
+    path(
+        "cafes/filter/",
+        cafe_views.filter_cafes_by_labels,
+        name="filter_cafes_by_labels",
+    ),
 ]
