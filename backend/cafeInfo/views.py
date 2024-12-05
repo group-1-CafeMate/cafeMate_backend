4d293bdba0b98297e487c6b2349ba80b8aa43ee0
Jerry-LIn <jarry0905337377@gmail.com>
2024-12-05 21:36:14 +0800

    features: add the get_top_cafes function

--- backend/cafeInfo/views.py
+++ backend/cafeInfo/views.py
@@ -71,4 +71,30 @@ def get_cafe(request):
     except Exception as e:
         return JsonResponse({"message": str(e), "success": False}, status = 500)
 
+@require_http_methods(["GET"])
+def get_top_cafes(request):
+    try:
+        # 按照 ig_post_cnt 降序("-"表降序)排列，取前六名
+        top_cafes = Cafe.objects.filter(legal=True).order_by('-ig_post_cnt')[:6]
 
+        if not top_cafes.exists():
+            return JsonResponse({'message': 'No cafes found', 'success': False}, status=404)
+
+        partial_cafe_info = [
+            {
+                'cafe_id': str(cafe.cafe_id),
+                'name': cafe.name,
+                'grade': cafe.grade,
+                'open_hour': cafe.open_hour,
+                'open_now': cafe.open_now,
+                'distance': cafe.distance,
+                'labels': cafe.get_labels(),
+                'image_url': cafe.images.all()[0].image.url if cafe.images.exists() else None
+            }
+            for cafe in top_cafes
+        ]
+
+        return JsonResponse({'cafes': partial_cafe_info, 'success': True}, safe=False, status=200)
+
+    except Exception as e:
+        return JsonResponse({"message": str(e), 'success': False}, status=500)
