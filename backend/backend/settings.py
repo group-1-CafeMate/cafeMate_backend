5cb8269fe3c9e135eaea7985f8f9842ab1c4b08a
Jefferson Chen <f1jefferson@yahoo.com.tw>
2025-01-01 19:56:33 +0800

    update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -250,7 +250,8 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "app/media")
+MEDIA_ROOT = "/app/media"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
